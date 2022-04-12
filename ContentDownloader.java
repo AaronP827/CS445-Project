602517470695628f3bf64065fa1a8f489ac391b4
apaprocki0827 <86206090+AaronP827@users.noreply.github.com>
2022-04-12 14:18:11 -0500

    Adding some basic methods and a new class to download information

---
+++ ContentDownloader.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class ContentDownloader {
+    private String urlStart = "http://localhost:8080/bn/api";
+
+    public Users createUser(){
+
+    }
+    public void activateUser(){
+
+    }
+    public Users updateUser(){
+
+    }
+    public void deleteUser(){
+
+    }
+    public ArrayList<Users> viewAllUsers(){
+
+    }
+    public Users viewUser(){
+
+    }
+    public ArrayList<Users> searchUsers(){
+
+    }
+
+    public Ask createAsk(){
+
+    }
+    public void activateAsk(){
+
+    }
+    public Ask updateAsk(){
+
+    }
+    public void deleteask(){
+
+    }
+    public ArrayList<Ask> viewAllAsks(){
+
+    }
+    public Ask viewAsk(){
+
+    }
+    public ArrayList<Ask> searchAsks(){
+        
+    }
+}
